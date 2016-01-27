c7504838950da87d8171dbdb851682c09af28c23
Laura Chacon Chacon <laura.chacon@c6s301pc43.fib.upc.es>
2016-01-27 13:38:26 +0100

    Error handler implemented

--- latitute_longitud_city/latitute_longitud_city.py
+++ latitute_longitud_city/latitute_longitud_city.py
@@ -1,14 +1,14 @@
 import sys, os.path
 from geopy.geocoders import Nominatim
-#from geopy.exc import GeocoderTimedOut
 
 if len(sys.argv) > 2:
-    city_name = sys.argv[1]
-    country_name = sys.argv[2]
+    city = sys.argv[1]
+    country = sys.argv[2]
     geolocator = Nominatim()
-    location = geolocator.geocode(city_name + " " + country_name)
-    print location[1]
-    #except GeocoderTimedOut as e:
-    #    print "input doesn't exist"
+    location = geolocator.geocode(city + " " + country)
+    if location is None:
+        print location[1]
+    else:
+        print "invalid value"
 else:
     print "invalid number of arguments"
