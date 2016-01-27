9e3e7027c12cd622e4c69c0d60df302890d7ddd1
Laura Chacon Chacon <laura.chacon@c6s301pc43.fib.upc.es>
2016-01-27 13:08:25 +0100

    Printing latitude and longitud working

---
+++ README.md
@@ -0,0 +1,19 @@
+# Learning Python
+## How to run the programs
+```
+$ python text_file_stats/text_file_stats.py text_file_stats/input.txt
+{
+   "characters": 916,
+   "lines": 5,
+   "most_common_letter": "e",
+   "most_common_word": "de",
+   "word_count": {
+       "Actualmente": 1,
+       "De": 1,
+       "Facebook": 1,
+       "Normalmente": 1,
+       ...
+   },
+   "words": 135
+}
+```
--- latitute_longitud_city/latitute_longitud_city.py
+++ latitute_longitud_city/latitute_longitud_city.py
@@ -1,15 +1,14 @@
 import sys, os.path
 from geopy.geocoders import Nominatim
-from geopy.exc import GeocoderTimedOut
+#from geopy.exc import GeocoderTimedOut
 
 if len(sys.argv) > 2:
     city_name = sys.argv[1]
     country_name = sys.argv[2]
     geolocator = Nominatim()
-    try: 
-        location = geolocator.geocode(city_name + " " + country_name)
-        print location[1]
-    except GeocoderTimedOut as e:
-        print "input doesn't exist"
+    location = geolocator.geocode(city_name + " " + country_name)
+    print location[1]
+    #except GeocoderTimedOut as e:
+    #    print "input doesn't exist"
 else:
     print "invalid number of arguments"
