f934bc7dfbb29b72d92b3733b5babff388e69ed4
Laura Chacon Chacon <laura.chacon@c6s301pc43.fib.upc.es>
2016-01-27 15:13:15 +0100

    Changed to json objects

--- latitute_longitud_city/latitute_longitud_city.py
+++ latitute_longitud_city/latitute_longitud_city.py
@@ -1,14 +1,17 @@
-import sys, os.path
+import sys, os.path, json
 from geopy.geocoders import Nominatim
 
 if len(sys.argv) > 2:
     city = sys.argv[1]
     country = sys.argv[2]
+    data = {}
     geolocator = Nominatim()
     location = geolocator.geocode(city + " " + country)
     if location is None:
-        print location[1]
+        data['error'] = "invalid value"
     else:
-        print "invalid value"
+        data['latitude'] = location[1][0]
+        data['longitud'] = location[1][1]
+    print json.dumps(data, indent=4, sort_keys=True)
 else:
     print "invalid number of arguments"
